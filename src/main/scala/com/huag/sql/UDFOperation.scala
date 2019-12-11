139c5bf7821ef75d6ea2dd0b03152ea681efcb12
huag <18600132770@163.com>
2019-12-11 18:17:07 +0800

    UDF注册后也可以在spark sql里直接用

---
+++ src/main/scala/com/huag/sql/UDFOperation.scala
@@ -0,0 +1,28 @@
+package com.huag.sql
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author huag
+  * @date 2019/12/11 18:11
+  */
+object UDFOperation extends App{
+
+  val spark = SparkSession
+    .builder()
+    .master("local")
+    .appName("UDFOperation")
+    .getOrCreate()
+
+  //自定义函数，可以在spark sql里直接用
+  def lengthLongerThan(str: String, length: Int): Boolean = str.length > length
+
+  spark.udf.register("lengthLongerThan", lengthLongerThan _)
+
+  val dataset = spark.read.json("/DataAnalyse/测试数据/students.json")
+
+  dataset.createOrReplaceTempView("student")
+
+  spark.sql("select name, lengthLongerThan(name, 3) from student").show()
+
+}
