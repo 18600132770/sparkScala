bbbb3151d685ceac6e2239bc4b9e2c47f18aeb57
huag <18600132770@163.com>
2019-12-11 16:07:16 +0800

    spark与hive的交互

--- src/main/scala/com/huag/sql/HiveDataSource.scala
+++ src/main/scala/com/huag/sql/HiveDataSource.scala
@@ -18,10 +18,28 @@ object HiveDataSource {
       .enableHiveSupport()
       .getOrCreate()
 
+//    spark.sql("select * from kg.nlp_doc").show()
+
+    spark.sql("CREATE DATABASE IF NOT EXISTS test")
+    spark.sql("USE test")
+
+//    spark.sql("DROP TABLE IF EXISTS userLowCarbon")
+    spark.sql("CREATE TABLE IF NOT EXISTS userLowCarbon (" +
+      "user_id string," +
+      "data_dt string," +
+      "low_carbon int" +
+      ")" +
+      "row format delimited fields terminated by '\\t'")
+
+//    spark.sql("LOAD DATA LOCAL INPATH  '/home/spark-study/hive/user_low_carbon.txt' INTO TABLE userLowCarbon")
+
+    val dataset = spark.sql("SELECT * from userLowCarbon")
+
+    dataset.createOrReplaceGlobalTempView("userLowCarbon")
+
+    spark.sql("select * from global_temp.userLowCarbon").show()
 
 
-
-    spark.sql("select * from kg.nlp_doc").show()
 
   }
 
